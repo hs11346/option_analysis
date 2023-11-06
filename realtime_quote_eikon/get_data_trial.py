6d38164a0208409cb62e82af6243f02e94d87905
Harry See <harry.see@legendarbgroup.com>
2023-11-06 14:23:37 +0800

    Merge from legend arb, include streaming interact

--- realtime_quote_eikon/get_data_trial.py
+++ realtime_quote_eikon/get_data_trial.py
@@ -4,5 +4,8 @@ import numpy as np
 
 ek.set_app_key('166a7ffcef9b411ba878d7145ca2f55c78aa4b02')
 list_ = ['0#IBM*.U','0#SPY*.U']
-fields = ['CF_EXCHANGE','CF_DATE','CF_TIME','CF_NAME', 'PUTCALLIND', 'STRIKE_PRC','EXPIR_DATE','CF_LAST', 'TR_BIDPRICE','TR_ASKPRICE','IMP_VOLT', 'OPINT_1','CF_VOLUME']
+fields = ['CF_EXCHNG','CF_DATE','CF_NAME', 'PUTCALLIND', 'STRIKE_PRC','EXPIR_DATE'\
+    ,'CF_LAST','IMP_VOLT', 'OPINT_1']
 option_chain,err = ek.get_data('0#SPY*.U', fields)
+
+#,'TR.DELTA','TR.GAMMA','TR.THETA','TR.VEGA'
